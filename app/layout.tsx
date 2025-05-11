7b211fd869e432cd7c83f859d1e9041e43381ec5
Ronak Bothra <ronakbothraa@gmail.com>
2025-05-11 17:44:57 +0530

    used but never defined error fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {
   ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
   UserButton,
 } from "@clerk/nextjs";
 import { ThemeProvider } from "next-themes";
