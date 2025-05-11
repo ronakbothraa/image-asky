f62b13bc40cc5e68d2b5bd1d35e0c985683f4f69
Ronak Bothra <ronakbothraa@gmail.com>
2025-05-11 13:13:43 +0530

    more fixes for deployment

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,9 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    '@tailwindcss/postcss': {}, // Use the new package here
+    autoprefixer: {},
+    // other plugins can also be listed here
+  },
 };
 
 export default config;
